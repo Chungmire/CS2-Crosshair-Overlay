dfb397f62c519c5d07c82bf7d6dfd8b42c8001a0
Chungmire <singermanm@ufl.edu>
2024-02-18 15:04:16 -0500

    First commit

---
+++ Crosshair/CrosshairInfo.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+using System.Text.RegularExpressions;
+
+namespace ShareCode
+{
+    enum CrosshairStyle
+    {
+        Default,
+        DefaultStatic,
+        Classic,
+        ClassicDynamic,
+        ClassicStatic,
+    }
+
+    struct CrosshairInfo
+    {
+        public CrosshairStyle Style; // LODWORD [14] >> 1
+        public bool HasCenterDot; // HIDWORD [14] & 1
+
+        public float Length; // [15] / 10
+        public float Thickness; // [13] / 10
+        public float Gap; // unchecked((sbyte) [3]) / 10
+
+        public bool HasOutline; // [11] & 8
+        public float Outline; // [4] / 2
+
+        public int Red; // [5]
+        public int Green; // [6]
+        public int Blue; // [7]
+
+        public bool HasAlpha; // HIDWORD [14] & 4
+        public int Alpha; // [8]
+
+        public int SplitDistance; // [9]
+        public float InnerSplitAlpha; // HIDWORD [11] / 10
+        public float OuterSplitAlpha; // LODWORD [12] / 10
+        public float SplitSizeRatio; // HIDWORD [12] / 10
+
+        public bool IsTStyle; // HIDWORD [14] & 8
+
+        public static CrosshairInfo Decode(byte[] bytes)
+        {
+            return new CrosshairInfo
+            {
+                Outline = (float)(bytes[4] / 2.0),
+                Red = bytes[5],
+                Green = bytes[6],
+                Blue = bytes[7],
+                Alpha = bytes[8],
+                SplitDistance = bytes[9],
+
+                InnerSplitAlpha = (float)((bytes[11] >> 4) / 10.0),
+                HasOutline = (bytes[11] & 8) != 0,
+                OuterSplitAlpha = (float)((bytes[12] & 0xF) / 10.0),
+                SplitSizeRatio = (float)((bytes[12] >> 4) / 10.0),
+
+                Thickness = (float)(bytes[13] / 10.0),
+                Length = (float)(bytes[15] / 10.0),
+                Gap = (float)(unchecked((sbyte)bytes[3]) / 10.0),
+
+                HasCenterDot = ((bytes[14] >> 4) & 1) != 0,
+                HasAlpha = ((bytes[14] >> 4) & 4) != 0,
+                IsTStyle = ((bytes[14] >> 4) & 8) != 0,
+
+                Style = (CrosshairStyle)((bytes[14] & 0xF) >> 1),
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Style)}: {Style}, {nameof(HasCenterDot)}: {HasCenterDot}, {nameof(Length)}: {Length}, {nameof(Thickness)}: {Thickness}, {nameof(Gap)}: {Gap}, {nameof(HasOutline)}: {HasOutline}, {nameof(Outline)}: {Outline}, {nameof(Red)}: {Red}, {nameof(Green)}: {Green}, {nameof(Blue)}: {Blue}, {nameof(HasAlpha)}: {HasAlpha}, {nameof(Alpha)}: {Alpha}, {nameof(SplitDistance)}: {SplitDistance}, {nameof(InnerSplitAlpha)}: {InnerSplitAlpha}, {nameof(OuterSplitAlpha)}: {OuterSplitAlpha}, {nameof(SplitSizeRatio)}: {SplitSizeRatio}, {nameof(IsTStyle)}: {IsTStyle}";
+        }
+    }
+
+    public class ShareCode
+    {
+        const string DICTIONARY = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefhijkmnopqrstuvwxyz23456789";
+        const string SHARECODE_PATTERN = "^CSGO(-?[\\w]{5}){5}$";
+
+        /// <summary>
+        /// Decode a share code from the string.
+        /// </summary>
+        /// <param name="shareCode"></param>
+        /// <returns></returns>
+        public static byte[] Decode(string shareCode)
+        {
+            var r = new Regex(SHARECODE_PATTERN);
+            if (!r.IsMatch(shareCode))
+                throw new Exception();
+
+            var code = shareCode.Remove(0, 4).Replace("-", "");
+
+            var big = BigInteger.Zero;
+            foreach (var c in code.ToCharArray().Reverse())
+            {
+                big = BigInteger.Multiply(big, DICTIONARY.Length) + DICTIONARY.IndexOf(c);
+            }
+
+            var all = big.ToByteArray().ToArray();
+            // sometimes the number isn't unsigned, add a 00 byte at the end of the array to make sure it is
+            if (all.Length == 18)
+                all = all.Concat(new byte[] { 0 }).ToArray();
+            return all.Reverse().ToArray();
+        }
+    }
+
+    //static class Program
+    //{
+       // static void DumpCode(string code)
+        //{
+        //    var info = CrosshairInfo.Decode(ShareCode.Decode(code));
+        //    Console.WriteLine($"{code}: {info}");
+       // }
+
+       // static void Main(string[] args)
+       // {
+           // DumpCode("CSGO-zpstH-jozpK-AxiWq-GNC2a-pVnMC");
+       // }
+ //   }
+}
---
+++ Crosshair/CrosshairSettings.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+internal class CrosshairSettings
+{
+    private static readonly Dictionary<float, int> LengthMap = new Dictionary<float, int>
+    {
+        {0, 0}, {0.3f, 1}, {0.7f, 2}, {1.2f, 3}, {1.6f, 4}, {2.1f, 5}, {2.5f, 6}, {2.9f, 7},
+        {3.399f, 8}, {3.799f, 9}, {4.299f, 10}, {4.699f, 11}, {5.199f, 12}, {5.599f, 13}, {5.999f, 14},
+        {6.499f, 15}, {6.899f, 16}, {7.399f, 17}, {7.799f, 18}, {8.299f, 19}, {8.699f, 20}, {9.199f, 21},
+        {9.599f, 22}, {10.099f, 23}
+    };
+
+
+    private static readonly Dictionary<float, int> ThicknessMap = new Dictionary<float, int>
+    {
+        {0, 1}, {0.7f, 2}, {1.12f, 3}, {1.56f, 4}, {2.01f, 5}, {2.45f, 6}, {2.89f, 7}
+    };
+
+
+    private static readonly Dictionary<float, int> GapMap = new Dictionary<float, int>
+    {
+        {-7.0f, -3}, {-5.9f, -2},{-4.9f, -1}, {-3.0f, 0}, {-2.0f, 1}, {-1.0f, 2}, {-0.9f, 2}, {-0.8f, 2}, {-0.7f, 2}, {-0.6f, 2}, {-0.5f, 2},{-0.4f, 2}, {-0.3f, 2}, {-0.2f, 2}, {-0.1f, 2}, {-0.05f, 3},{0.00f, 3}, {1.0f, 4},
+        {2.0f, 5}, {3.0f, 6}, {4.0f, 7}
+    };
+
+    internal static int GetPixelLength(float settingValue)
+    {
+        return GetMappedValue(LengthMap, settingValue);
+    }
+
+    internal static int GetPixelThickness(float settingValue)
+    {
+        return GetMappedValue(ThicknessMap, settingValue);
+    }
+
+    internal static int GetPixelGap(float settingValue)
+    {
+        return GetMappedValue(GapMap, settingValue);
+    }
+
+    private static int GetMappedValue(Dictionary<float, int> map, float settingValue)
+    {
+        var closestKey = map.Keys.Where(k => k <= settingValue).DefaultIfEmpty(-1).Max();
+
+        if (closestKey == -1)
+        {
+            return 0; // Or some default value
+        }
+
+        return map[closestKey];
+    }
+}
